0ef05f8ba1e2370b8d5c088955eaa3e660547693
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-13 21:27:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+
+
+class School
+  attr_accessor :name, :roster
+  attr_reader 
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end  
+  def add_student(stud_name, stud_grade)
+    @roster[stud_grade] ||= []
+    @roster[stud_grade] << stud_name
+  end  
+ def grade(stud_grade)
+    
+ end   
+  
+  
+end  
